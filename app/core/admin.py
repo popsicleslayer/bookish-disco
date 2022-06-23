53767bd9f70e441880ccf22e9e1ddacb506649c6
Anastazja <anastazja.ada@gmail.com>
2022-06-23 10:44:04 +0200

    Added add user page  and tests to Admin Page

--- app/core/admin.py
+++ app/core/admin.py
@@ -27,5 +27,20 @@ class UserAdmin(BaseUserAdmin):
         (_('Important dates'), {'fields': ('last_login',)}), #This comma after last_login IS important
     )
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+
 
 admin.site.register(models.User, UserAdmin)
