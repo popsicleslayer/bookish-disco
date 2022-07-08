c562d4e879b1f7bbde7643dfe92d2e395d26cb58
Anastazja <anastazja.ada@gmail.com>
2022-07-08 12:16:42 +0200

    Added serializer and view for image upload

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -96,3 +96,13 @@ class RecipeDetailSerializer(RecipeSerializer):
 
         instance.save()
         return instance
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading iamges to recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ['id','image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
