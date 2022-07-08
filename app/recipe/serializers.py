180b2e85de68c616243827e913f2a50ca500ae6f
Anastazja <anastazja.ada@gmail.com>
2022-07-08 13:41:21 +0200

    Added image to detail recipe serializer

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -103,6 +103,6 @@ class RecipeImageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ['id','image']
+        fields = ['id', 'image']
         read_only_fields = ['id']
         extra_kwargs = {'image': {'required': 'True'}}
