630c04bbff88d1472249eb8ff42f1028fa12eada
Anastazja <anastazja.ada@gmail.com>
2022-07-08 13:39:14 +0200

    Added image to detail recipe serializer

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -47,7 +47,7 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for a detial view of a recipe"""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
 
     def _get_or_create_tags(self, tags, recipe):
         """Handle getting or creating tags as needed"""
